32ad868455d39c9678559da42cb079c175a93832
aavail007 <aavail007@gmail.com>
2020-02-06 21:45:21 +0800

    init

---
+++ F2E_MaskMap.js
@@ -0,0 +1,12 @@
+$(function () {
+
+
+});
+// =========================
+_.templateSettings = { //  underscore.js .Template method.
+    evaluate: /\{\{(.+?)\}\}/g,
+    interpolate: /\{\{=(.+?)\}\}/g,
+    escape: /\{\{-(.+?)\}\}/g
+};
+
+
